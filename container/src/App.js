1e952fb9eb807cb6c83703053777c47c042c703c
talesMIU <talessmiura@gmail.com>
2025-09-07 11:19:17 -0300

    container app

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CMercadoApp from './components/CMercadoApp';
+
+export default () => {
+    return (
+        <div>
+            <hr />
+            <CMercadoApp />
+        </div>
+    );
+};
